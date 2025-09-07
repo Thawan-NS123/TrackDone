6c87f9b9bbdc31211b68a01154fca491b9c85003
luanreinhold <104692963+luanreinhold@users.noreply.github.com>
2025-09-07 19:03:04 -0300

    other: ordem register

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,9 @@ import { userRoutes } from './controllers/user/user-routes';
 import { taskRoutes } from './controllers/task/task-routes';
 
 const server: FastifyInstance = fastify();
+server.register(cors, { origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'], credentials: true });
 server.register(userRoutes, { prefix: '/user' });
 server.register(taskRoutes, { prefix: '/task' });
-server.register(cors, { origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'], credentials: true });
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
