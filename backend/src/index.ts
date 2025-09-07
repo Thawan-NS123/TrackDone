f93601943a314ed8dec012f82aeeac453a8cc43b
luanreinhold <104692963+luanreinhold@users.noreply.github.com>
2025-09-07 18:58:09 -0300

    feat: CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+import cors from '@fastify/cors';
 import fastify, { type FastifyInstance } from 'fastify';
 import { userRoutes } from './controllers/user/user-routes';
 import { taskRoutes } from './controllers/task/task-routes';
@@ -5,6 +6,7 @@ import { taskRoutes } from './controllers/task/task-routes';
 const server: FastifyInstance = fastify();
 server.register(userRoutes, { prefix: '/user' });
 server.register(taskRoutes, { prefix: '/task' });
+server.register(cors, { origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'], credentials: true });
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
