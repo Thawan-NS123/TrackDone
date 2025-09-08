7239908e28848829e133875b8ef6af04fea0d37f
Thawan-NS123 <thawann.silva34@gmail.com>
2025-09-08 18:01:09 -0300

    Adicionando a ida para o calendario do google

--- frontend/src/components/HeaderComponent/index.js
+++ frontend/src/components/HeaderComponent/index.js
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import React from 'react';
 import './index.css';
-import { FaTasks, FaUser, FaCog, FaChartLine } from 'react-icons/fa';
+import { FaTasks, FaCalendarAlt, FaCog, FaChartLine } from 'react-icons/fa';
 
 export default function Header() {
   const navigate = useNavigate();
@@ -14,7 +14,7 @@ export default function Header() {
 
       <nav className="nav-icons">
         <FaTasks className="icon" title="Tarefas" onClick={() => navigate('/')} />
-        <FaUser className="icon" title="Cadastro de Usuário" onClick={() => navigate('/colaborador')} />
+        <FaCalendarAlt className="icon" title="Calendário" onClick={() => window.open("https://calendar.google.com", "_blank")} />
         <FaChartLine className="icon" title="Relatórios" onClick={() => navigate('/relatorio')} />
         <FaCog className="icon" title="Configurações" onClick={() => navigate('/config')} />
       </nav>
